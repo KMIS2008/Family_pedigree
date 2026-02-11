f00e0caf646c0018e47632ab1783e9663390dac9
KMIS2008 <iryna.makarenko@charityhero.live>
2026-02-11 19:06:00 +0200

    setting BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
     <PersistGate  persistor={persistor}>
-      <BrowserRouter  basename="/Temple-App.-Peter-and-Paul-">
+      <BrowserRouter  basename="/Family_pedigree">
           <ThemeProvider theme = {theme}>
              <App />
           </ThemeProvider>
